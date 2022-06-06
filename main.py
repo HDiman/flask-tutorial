da2b5cfabedc28b85b3ede176fa4902bae55844a
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-06-07 00:13:06 +0300

    Redirect to new page

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello</h1> <a href='https://www.yahoo.com/'>yahoo.com</a>"
+    return "<h1>Welcome back</h1>"
 
 @app.route("/<name>")
 def company_page(name):
     return f"<h2>Here is a link to:<h2/> <a href='https://www.{name}.com/'>{name}</a>"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
