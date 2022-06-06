117a5d5d58498712831a3c56b08e9509792a4caa
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-06-07 00:07:43 +0300

    Adding new web pages with different functions

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "<h1>Hello</h1> <a href='https://www.yahoo.com/'>yahoo.com</a>"
+
+@app.route("/<name>")
+def company_page(name):
+    return f"<h2>Here is a link to:<h2/> <a href='https://www.{name}.com/'>{name}</a>"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
