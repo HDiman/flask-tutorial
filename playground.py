21d9c63a14bfd897b401e6ed4e6ec56b1ae56a7c
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-06-08 09:42:16 +0300

    Session

--- playground.py
+++ playground.py
@@ -18,7 +18,9 @@ def admin():
 if __name__ == "__main__":
     app.run(debug=True)
 
+
 # ======================================================================
+
 
 from flask import Flask, redirect, url_for, render_template
 import random
@@ -32,6 +34,34 @@ def home():
 @app.route("/<name>")
 def index(name):
     return render_template("index.html", name=name, rnd=random.randint(1, 10))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# ======================================================================
+
+
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user_name = request.form["nm"]
+        return redirect(url_for("user", usr=user_name))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
